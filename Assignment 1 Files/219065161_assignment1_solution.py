a96ec700e3c73c62d722a2b23297f79dd4876e23
Daniel Matar <dmatar@deakin.edu.au>
2023-03-30 21:13:00 +1100

    Updated code
    Updated code using PBL Task 2 Knights Tour from SIT215 2021(2022)
    
    Code included in this aswell as reference (Inside Assignment 1/Resources/PBL Task 2)

--- Assignment 1 Files/219065161_assignment1_solution.py
+++ Assignment 1 Files/219065161_assignment1_solution.py
@@ -1,67 +1,43 @@
 """
 Solution for SIT215: Computational Intelligence Task 1
 """
-
           
 def findStart():
     for i in range(len(maze)):
         for j in range(len(maze[0])):
             if maze[i][j] == "S": # Starting position
-                DFS(i,j)      
+                DFS(maze, i,j, visited)      
 
-
-# Depth-First Search using starting pos and end pos
-def DFS(r, c):
-    # Check if we have reached the end point
-    if maze[r][c] == "E":
-        return True
-    if maze[r][c-1] == " ":
-        maze[r][c-1] = "*"
-        if DFS(r-1, c):
+def isSafe(maze, r, c, visited):
+    if r >= 0 and r < len(maze) and c >= 0 and c < len(maze[0]):
+        if maze[r][c] != "#" and not visited[r][c]:
             return True
-        else:
-            maze[r][c-1] == ' '
-            DFS(r, c-1)
-            return False
-
-    if maze[r-1][c] == " ":
-        maze[r-1][c] = "*"
-        if DFS(r-1, c):
-            return True
-        else:
-            maze[r-1][c] == ' '
-            DFS(r-1,c)
-            return False
-
-    if maze[r+1][c] == " ":
-        maze[r+1][c] == '*'
-        if DFS(r+1, c):
-            return True
-        else:
-            maze[r+1][c] == ' '
-            DFS(r+1, c)
-            return False
-
-
-    if maze[r][c+1] == " ":
-        maze[r][c+1] == '*'
-        if DFS(r, c+1):
-            return True
-        else:
-            maze[r][c+1] == ' '
-            DFS(r, c+1)
-            return False
-
-    
-    # If no path is found, return False
     return False
 
 
+# Depth-First Search using starting pos and end pos
+def DFS(maze, r, c, visited):
+    # Check if we have reached the end point
+    if maze[r][c] == "E":
+        return True
+    
+    visited[r][c] = True
 
+    # Loop for north, south, east, and west sections
+    for row, col in [(1,0),(-1,0),(0,1),(0,-1)]:
+        newR, newC = r + row, c + col
+        if isSafe(maze, newR, newC, visited):
+            
+            if DFS(maze, newR, newC, visited):
+                maze[newR][newC] = "*"
+                return True
+    return False
+    
 
 # Reads maze configuration from input.txt file
 with open('Assignment 1 Files\input.txt') as f:
         maze = [list(line.strip()) for line in f]
+        visited = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
         findStart()
         for row in maze:
             for char in row:
@@ -71,6 +47,28 @@ with open('Assignment 1 Files\input.txt') as f:
                     print(char, end="")
             print()
 
-
-
-    
+# Reads maze configuration from input.txt file
+with open('Assignment 1 Files\input2.txt') as f:
+        maze = [list(line.strip()) for line in f]
+        visited = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
+        findStart()
+        for row in maze:
+            for char in row:
+                if char == "*":
+                    print("\033[91m{}\033[0m".format(char), end="")
+                else:
+                    print(char, end="")
+            print()
+            
+# Reads maze configuration from input.txt file
+with open('Assignment 1 Files\input3.txt') as f:
+        maze = [list(line.strip()) for line in f]
+        visited = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
+        findStart()
+        for row in maze:
+            for char in row:
+                if char == "*":
+                    print("\033[91m{}\033[0m".format(char), end="")
+                else:
+                    print(char, end="")
+            print()
